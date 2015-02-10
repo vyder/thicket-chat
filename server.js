11b00ff8530811b0ce0433d847ce8ce2da2422d5
Vidur Murali <vidur.murali@gmail.com>
2015-02-10 23:44:55 +0530

    Add express, create dummy server in ChatApp

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var _            = require("underscore"),
     Promise      = require("bluebird"),
+    express      = require("express"),
     thicket      = require("thicket"),
     Logger       = thicket.c("logger"),
     CLA          = thicket.c("appenders/console-log"),
@@ -13,19 +14,29 @@ var ChatApp = function() { this.initialize.apply(this, arguments); };
 _.extend(ChatApp.prototype, App.prototype, {
   initialize: function() {
     App.prototype.initialize.apply(this, arguments);
+    this._expressApp = express();
   },
-  up: Promise.method(function() {
-    Log.debug("Look at me, doing some meaningful setup work...");
+  up: Promise.method(function(configuration) {
+    Log.debug("Setting up default route");
+    this._expressApp.get('/', function(req, res){
+      res.send('hello world');
+    });
+
+    var port = parseInt(this._config['port']);
+
+    this._server = this._expressApp.listen(port);
+    Log.debug("Listening at http://127.0.0.1:" + port);
   }),
   down: Promise.method(function() {
-    Log.debug("Look at me, doing some meaningful teardown work...");
+    Log.debug("Shutting down the server...");
+    this._server.close();
   })
 });
 
 Logger.root().setLogLevel(Logger.Level.Debug);
 Logger.root().addAppender(new CLA());
 
-var bootstrapper = new Bootstrapper({applicationConstructor: ChatApp});
+var bootstrapper = new Bootstrapper({ applicationConstructor: ChatApp });
 bootstrapper
   .bootstrap()
   .then(function(appContainer) {
